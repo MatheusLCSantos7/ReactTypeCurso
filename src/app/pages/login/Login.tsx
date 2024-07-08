70ead800d92d1b79b7b51738c9cb029e9c0493ae
matheussantos <matheus.santos@microuniverso.com.br>
2024-07-08 10:11:18 -0300

    Feat; Tela login e form login

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,28 +1,43 @@
+import { useState } from "react";
 import { useNavigate   } from "react-router-dom";
 
+
+
 export  const Login = () => {
+
+    const  [email, setEmail] = useState('');
+    const  [senha, setSenha] = useState('');
+
+
+
+
+
     const history = useNavigate  ();
 
     const handleclick = () =>{
         history("/pagina-inicial");
     }
+
     return(
         <>
             <form action="">
+                
                 <label htmlFor="">
                     <span>Email</span>
-                    <input/>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
                 </label>
 
 
                 <label htmlFor="">
                     <span>Senha</span>
-                    <input type="password"/>
+                    <input type="password" value={senha} onChange={e => setSenha(e.target.value)}/>
                 </label>
+
+                <button onClick={handleclick}>Voltar para Pagina Inicial</button>
+            
             </form>
 
         
-        <button onClick={handleclick}>Voltar para Pagina Inicial</button>
         </>
     );
 }
